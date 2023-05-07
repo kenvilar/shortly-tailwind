9ce2df1d94cc8b6fb9d33a39bcfec25dc02af26f
kenvilar <kenvilar@gmail.com>
2023-05-07 18:14:09 +0800

    setup tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        cyan: "hsl(180, 66%, 49%)",
+        cyanLight: "hsl(180, 66%, 69%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+        red: "hsl(0, 87%, 67%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%) ",
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      spacing: {
+        180: "32rem",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
