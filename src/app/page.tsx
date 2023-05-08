0b003bf862fdafe8d5792b8a8017a738e9262265
kenvilar <kenvilar@gmail.com>
2023-05-08 15:38:55 +0800

    implement Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Nav from "@/app/components/Nav";
+import Hero from "@/app/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Nav />
+
+      <Hero />
     </>
   );
 }
