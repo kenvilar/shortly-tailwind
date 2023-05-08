eda63047906f5995b7a553c2aeefbb5833990768
kenvilar <kenvilar@gmail.com>
2023-05-08 16:51:13 +0800

    add cta section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/app/components/Hero";
 import Shorten from "@/app/components/Shorten";
 import Stats from "@/app/components/Stats";
 import Features from "@/app/components/Features";
+import CTA from "@/app/components/CTA";
 
 export default function Home() {
   return (
@@ -16,6 +17,8 @@ export default function Home() {
       <Stats />
 
       <Features />
+
+      <CTA />
     </>
   );
 }
